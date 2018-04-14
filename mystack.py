582cfef6b63f10620bfded80d295dab5527e9986
Utkarsh Mishra <37778872+ulshell@users.noreply.github.com>
2018-04-14 12:11:40 +0530

    Update mystack.py

--- mystack.py
+++ mystack.py
@@ -1,9 +1,5 @@
 import click
 from services import *
-
-
-
-
 @click.command()
 @click.option('--screenshot', default='', help='Take a screenshot')
 @click.option('--search', default='', help='Type your query(text)')
